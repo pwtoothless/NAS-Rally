69fd181c6de5d7424a61dec5c2dd9c0390b62701
pwtoothless <pwtoothless63@gmail.com>
2026-05-31 19:30:04 -0700

    Initial Commit

---
+++ Rally Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rally Chat
+//
+//  Created by Peyton Ward on 5/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rally Chat/Rally_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rally_ChatApp.swift
+//  Rally Chat
+//
+//  Created by Peyton Ward on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Rally_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
